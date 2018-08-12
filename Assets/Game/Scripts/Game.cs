3165849bd6e9addab38e242b282e24749667f66c
upisfree <upisfree@outlook.com>
2018-08-12 20:27:00 +0600

    post-fair builds

--- Assets/Game/Scripts/Game.cs
+++ Assets/Game/Scripts/Game.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 
 public class Game : MonoBehaviour {
+    public bool isGameStarted = false;
     void Start () {
         SceneManager.DisableAll();
 
@@ -16,12 +17,19 @@ public class Game : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.R)) {
             UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
         }
+
+        if (isGameStarted && Input.anyKeyDown) {
+            GameObject.Find("UI").SetActive(false);
+            GameObject.Find("Player").GetComponent<FirstPersonController>().enabled = true;
+        }
 	}
 
     IEnumerator HideStartScreen() {
-        yield return new WaitForSeconds(7);
+        yield return new WaitForSeconds(1);
 
-        GameObject.Find("UI").SetActive(false);
-        GameObject.Find("Player").GetComponent<FirstPersonController>().enabled = true;
+        isGameStarted = true;
+
+        //GameObject.Find("UI").SetActive(false);
+        //GameObject.Find("Player").GetComponent<FirstPersonController>().enabled = true;
     }
 }
