225c06c4a485c2268bb6aa8ab25a3abcab9793f7
upisfree <upisfree@outlook.com>
2018-08-12 18:07:53 +0600

    fair build

--- Assets/Game/Scripts/Game.cs
+++ Assets/Game/Scripts/Game.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class Game : MonoBehaviour {
     void Start () {
         SceneManager.DisableAll();
 
-        SceneManager.Enable(SceneManager.Scenes[0]);		
+        SceneManager.Enable(SceneManager.Scenes[0]);
+
+        StartCoroutine(HideStartScreen());
 	}
 	
 	void Update () {
@@ -14,4 +17,11 @@ public class Game : MonoBehaviour {
             UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
         }
 	}
+
+    IEnumerator HideStartScreen() {
+        yield return new WaitForSeconds(7);
+
+        GameObject.Find("UI").SetActive(false);
+        GameObject.Find("Player").GetComponent<FirstPersonController>().enabled = true;
+    }
 }
